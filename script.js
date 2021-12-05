d033db4434c344d6f8baeb844b0670ea393a3988
Emanuele Guarnaccia <emanueleguarnaccia@gmail.com>
2021-12-05 12:34:21 +0800

    added the functionalities to remove books from the library and the dom and to toggle the read status; also improved the gui

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const gridContainer = document.querySelector(".grid-container");
-const newBookBtn = document.querySelector(".btn");
-const submitBtn = document.querySelector(".submit-button");
+const newBookBtn = document.querySelector(".btn").addEventListener("click", () => {
+    document.querySelector(".form").style.display = 'block';
+});
+const author = document.getElementById("author");
+const title = document.getElementById("title");
+const pages = document.getElementById("pages");
+const status = document.getElementById("status");
+const submitBtn = document.querySelector(".submit-button").addEventListener("click", (e) => {
+    e.preventDefault();
+    //push book into array
+    addBookToLibrary();
+    // show books in a grid
+    displayOnPage()
+    // reset form and hide
+    resetForm();
+})
 const form = document.querySelector(".form");
+
+
 
 let myLibrary = [];
 
@@ -10,95 +26,84 @@ function Book(Author, Title, Pages, Status) {
   this.Author = Author;
   this.Title = Title;
   this.Pages = Pages;
-  this.Status = Status;
-  
+  this.Status = Status;  
 }
-    
-// make form appear upon button click
-newBookBtn.addEventListener("click", () => {
-    document.querySelector(".form").style.display = 'block';
-})
 
 // push new book into array myLibrary
-function addBookToLibrary(newBook) {
-    myLibrary.push(newBook);
-    return myLibrary;
-  }
-
-
-// input new book with form 
-submitBtn.addEventListener("click", (e) => {
-
-    e.preventDefault();
-
-    let Author = document.getElementById("author").value;
-    let Title = document.getElementById("title").value;
-    let Pages = document.getElementById("pages").value;
-    let Status = document.getElementById("status").value;
+function addBookToLibrary() {
     
-    let newBook = new Book(Author, Title, Pages, Status);
+    let newBook = new Book(author.value, title.value, pages.value, status.value);
+    
+    myLibrary.push(newBook);
+    
+}
 
-    //push book into array
-    addBookToLibrary(newBook);
 
-    // show books in a grid
-    createNewGrid();
+function displayOnPage() {
+        gridContainer.innerHTML = '';
+        for (let i = 0; i < myLibrary.length; i++) {
+            gridContainer.innerHTML += `<div class="book-container" data-index="${i}">
+                <p>Title: ${myLibrary[i].Author}</p>
+                <p>Author: ${myLibrary[i].Title}</p>
+                <p>Pages: ${myLibrary[i].Pages}</p>
+                <p class="Status">Status: ${myLibrary[i].Status}</p>
+                <button class="change-status">Change Status</button>
+                <button class="remove-btn">Remove ✖️</button>
+            </div>`;
 
-})
+            
 
-function createNewGrid() {
-
-    for(books of myLibrary) {
-
-        // loop through array and create grid-items
-        const gridItem = document.createElement("div");
-        gridItem.classList.add("grid-item");
-        gridItem.dataset.test= "data";
-        
-        const bookTitle = document.createElement("h3");
-        bookTitle.classList.add("book-title");
-        bookTitle.textContent = `${"Book"}`;
-        gridItem.appendChild(bookTitle);
-
-        const book = document.createElement("div");
-        book.classList.add("book");
-        gridItem.appendChild(book);
-
-        const removeBtn = document.createElement("button");
-        removeBtn.classList.add("remove-button");
-        removeBtn.textContent = "remove from library";
-        gridItem.appendChild(removeBtn);
-
-        gridContainer.appendChild(gridItem);
-
-        for(let key in books) {
-            //loop through object properties and add book details to book item
-            if(Object.prototype.hasOwnProperty.call(books, key)) {
-                
-                const bookDetails = document.createElement("div");
-                bookDetails.classList.add("book-details");
-                bookDetails.textContent = key + ":" + " " + books[key];
-                book.appendChild(bookDetails);
-        
-            }
         }
-        // create button to change status of read
-        const changeStatus = document.createElement("button");
-        changeStatus.classList.add("change-status");
-        changeStatus.textContent = "change status"
-        book.appendChild(changeStatus);
-        
-        // remove grid item from array
-        removeBtn.addEventListener("click", () => {
-        gridItem.remove();    
-        })
 
-    }
-    // reset form, make its display to none again and empty the array
+       
+        
+        const removeButton = document.querySelectorAll(".remove-btn");
+        
+        removeButton.forEach((button, index) => {
+            button.addEventListener("click", (e) => {
+                
+                // remove the book object with index = index;
+                myLibrary.splice(index, 1);
+                // remove the bookContainer whose data-index value is equal to index;
+                e.target.parentNode.remove();
+                
+            })
+        })  
+        
+        const changeStatus = document.querySelectorAll(".change-status");
+        changeStatus.forEach((button, index) => {
+            button.addEventListener("click", (e) => {
+                
+                if (myLibrary[index].Status == "Read" && e.target.previousElementSibling.innerText == "Status: Read") {
+                    myLibrary[index].Status = "Non read";
+                    e.target.previousElementSibling.innerText = "Status: Non read";
+                
+                } else if (myLibrary[index].Status == "Non read" && e.target.previousElementSibling.innerText == "Status: Non read") {
+                    myLibrary[index].Status = "Read"
+                    e.target.previousElementSibling.innerText = "Status: Read";
+                }
+                
+            })
+        })  
+
+}
+
+function resetForm() {
     form.reset();
     document.querySelector(".form").style.display = 'none';
-    myLibrary = [];
 }
+
+
+
+    
+
+
+/*you can querySelectorAll(".delete-book-btns") immeadiately after the for loop in renderDisplay(). Keep in mind that the forEach method can take a second parameter for index. That way it's easy to pass the index-number to deleteBook without any extra code. Example:  .forEach(item, index => item.addEventListener("click", () => fn(index))
+I wouldnt say it's the most stable way to do this though. Since you never know if, in future iterations of the code, your array gets modified before .forEach attaches the eventlisteners and hence be in danger that the indizes dont match anymore. But I guess it's no biggie for now. Just keep it in mind for future tasks like that*/    
+    
+
+
+
 
 
 
